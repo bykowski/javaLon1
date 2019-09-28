4da45683663853fae96fba4b58dc47855ae04d24
Przemyslaw Bykowski <p.bykowski@sdacademy.pl>
2019-09-28 12:31:38 +0100

    post

--- src/main/java/pl/bykowski/game/CatController.java
+++ src/main/java/pl/bykowski/game/CatController.java
@@ -3,22 +3,38 @@ package pl.bykowski.game;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CatController {
 
-    private Cat cat;
+    private List<Cat> catList;
 
     public CatController() {
-        cat = new Cat("Filemon", "Dachowiec", "#8B0000");
+        catList = new ArrayList<>();
+        catList.add(new Cat("Filemon", "Dachowiec", "white"));
+        catList.add(new Cat("Bonifacy", "Dachowiec", "black"));
+        catList.add(new Cat("Klakier", "Dachowiec", "#8B0000"));
     }
 
     @GetMapping("/cats")
     public String getCats(Model mode) {
-        mode.addAttribute("nameModel", cat.getName());
-        mode.addAttribute("breedModel", cat.getBreed());
-        mode.addAttribute("colorModel", cat.getColor());
+        mode.addAttribute("catListModel", catList);
         return "catsView";
     }
+
+    @PostMapping("/cats")
+    public String addCat(@RequestBody Cat cat) {
+        catList.add(cat);
+        return "redirect:/cats";
+    }
+
+
+
+
 
 }
